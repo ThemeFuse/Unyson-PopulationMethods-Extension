c5bfaaf9b848f1db25753386623543957f8a9180
moldcraft <moldcraft@email.com>
2016-10-03 13:48:41 +0300

    Version 1.0.18

--- manifest.php
+++ manifest.php
@@ -2,6 +2,6 @@
 
 $manifest['name']        = __( 'Population Methods', 'fw' );
 $manifest['description'] = '';
-$manifest['version'] = '1.0.17';
+$manifest['version'] = '1.0.18';
 
 $manifest['github_update'] = 'ThemeFuse/Unyson-PopulationMethods-Extension';
